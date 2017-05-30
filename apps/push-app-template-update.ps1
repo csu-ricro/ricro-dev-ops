5820ac6f730a64b344a96d73cfd3d0a65d30c3ec
Daniel Lennox <dlennox2424@gmail.com>
2017-05-30 14:56:58 -0600

    Adding push-app-template-update PowerShell script

--- apps/push-app-template-update.ps1
+++ apps/push-app-template-update.ps1
@@ -1,1 +1,44 @@
-"Hallo";
+$ProjectDirs = dir c:\dev\sandbox\apps | ?{$_.PSISContainer}
+$SourcePath = "c:\dev\sandbox\ricro-app-template\src\csu-app-template"
+$ProdPath = "c:\dev\sandbox\prod"
+
+$CopyLocation = "src\csu-app-template"
+$OriginalDir = $PWD.Path
+
+foreach ($app in $ProjectDirs) {
+    " "
+    Write-Host $app
+    $LocalDir = $app.FullName
+    Set-Location $app.FullName
+
+    # Check if the location exists and nuke it if it does
+    if(Test-Path "$LocalDir\$CopyLocation\") {
+        Remove-Item "$LocalDir\$CopyLocation\" -recurse
+    }
+
+    # Create location before copy
+    New-Item "$LocalDir\$CopyLocation\" -type directory
+
+    # Bundle parameters to copy command, add force if necessary
+    $splat = @{
+        Path = "$SourcePath\*"
+        Destination = ".\$CopyLocation\"
+        Recurse = $True
+    }
+
+    if($Force) {
+        $splat += @{Force = $True}
+    }
+
+    # Run the copy command
+    Copy-Item @splat
+
+    # Finally, run our npm command
+    npm run build
+
+    # Compress production build
+    Compress-Archive -Path "$LocalDir\build\*" -DestinationPath "$ProdPath\$app.zip" -Force
+
+    Write-Host $app finished rebuild -BackgroundColor DarkGreen
+}
+Set-Location $OriginalDir
